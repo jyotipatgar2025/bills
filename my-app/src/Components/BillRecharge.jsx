95329dcc5d5d776b78f362de515b79c95364e408
jyotipatgar2025 <jyotipatgar663@gmail.com>
2025-02-12 10:46:50 +0530

    Initial Commit

--- my-app/src/Components/BillRecharge.jsx
+++ my-app/src/Components/BillRecharge.jsx
@@ -9,7 +9,7 @@ const billServices = [
   { name: "DTH", icon: <FaSatelliteDish style={{ color: "#800080" }} /> }, // Purple
   { name: "Water", icon: <FaTint style={{ color: "#0088ff" }} /> }, // Blue
   { name: "Gas", icon: <FaGasPump style={{ color: "#ff4500" }} /> }, // Red-Orange
-  { name: "Insurances", icon: <FaUmbrella style={{ color: "#006400" }} /> }, // Green
+  { name: "Insurance", icon: <FaUmbrella style={{ color: "#006400" }} /> }, // Green
 ];
 
 const travelServices = [
@@ -17,23 +17,23 @@ const travelServices = [
   { name: "Bus", icon: <FaBus style={{ color: "#228b22" }} />, extra: "Affordable Rides" }, // Green
   { name: "Train", icon: <FaTrain style={{ color: "#8b4513" }} /> }, // Brown
   { name: "Hotel", icon: <FaHotel style={{ color: "#4b0082" }} />, extra: "Budget-friendly Stay" }, // Indigo
-  { name: "Car Rental", icon: <FaCar style={{ color: "#4682b4" }} />, extra: "Drive Easy Anywhere" }, // Steel Blue
+  { name: "Car Rentals", icon: <FaCar style={{ color: "#4682b4" }} />, extra: "Drive Easy Anywhere" }, // Steel Blue
 ];
 
 const Services = () => {
   return (
     <div className="container">
-      <div className="row">
+      <div className="rows">
         <div className="section">
           <div className="left-content">
             <h2 className="section-title">Bills & Recharge</h2>
-            <p className="description">Pay your bills & recharge instantly with Justdial</p>
+            <p className="description">Pay your bills & recharge instantly with the  Justdial</p>
             <h5>Explore More</h5>
           </div>
-          <div className="icons-grid">
+          <div className="icon-grid">
             {billServices.map((item, idx) => (
               <div key={idx} className="service-item">
-                <div className="icon-box">
+                <div className="icon-boxs">
                   <span className="icon">{item.icon}</span>
                 </div>
                 <p className="icon-text">{item.name}</p>
@@ -49,7 +49,7 @@ const Services = () => {
         <div className="section">
           <div className="left-content">
             <h2 className="section-title">Travel Bookings</h2>
-            <p className="description">Instant ticket bookings for your best travel experience</p>
+            <p className="description">Instant ticket booking for your best travel experience</p>
             <h5>Explore More</h5>
           </div>
           <div className="icon-grid">
